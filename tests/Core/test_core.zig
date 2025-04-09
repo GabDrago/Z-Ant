92ab734c41a7adbc25efad70cc5f2e3b9a342452
GabDrago <gabriele.drago03@gmail.com>
2025-04-09 15:54:58 +0200

    minmax tests and debug

--- tests/Core/test_core.zig
+++ tests/Core/test_core.zig
@@ -3,4 +3,5 @@ const std = @import("std");
 test {
     _ = @import("Tensor/test_tensor.zig");
     _ = @import("Quantization/test_clustering.zig");
+    _ = @import("Quantization/test_quantization.zig");
 }
