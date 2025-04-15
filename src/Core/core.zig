99b490e498d1b90b3912b9a767041f057c677a98
Samuele Succetti <samuelesuccetti02@gmail.com>
2025-04-15 19:33:54 +0000

    first effort wrapper

--- src/Core/core.zig
+++ src/Core/core.zig
@@ -1,4 +1,8 @@
 pub const tensor = @import("Tensor/tensor.zig");
+
 pub const quantization = @import("Quantization/quantization.zig");
 pub const clustering = @import("Quantization/clustering.zig");
 pub const clustering_debug = @import("Quantization/clustering_debug.zig");
+
+pub const classicTensor = @import("TensorRefactor/classicTensor.zig");
+pub const tensorWrapper = @import("TensorRefactor/tensorWrapper.zig");
